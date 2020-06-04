c3a309a027a15aea85dca0ee593ef174b84e4d72
OneKey <devonekey@gmail.com>
2020-06-05 02:24:30 +0900

    Main classes are modified for Programmers

--- src/Main.kt
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner
-
-class Main {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>): Unit = with(Scanner(System.`in`)) {
-        }
-    }
-}
---
+++ src/legacy/Solution.java
@@ -0,0 +1,6 @@
+package legacy;
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
